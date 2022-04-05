7d4a7b38e75df889f26879c2cc0350d8a86399e9
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 15:16:36 +0800

    add uid

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,10 @@
-import { Invoice } from "./classes/Invoice.js" // not TS but JS, otherwise browser does not know
-import { ListTemplate } from "./classes/ListTemplate.js";
-import { Payment } from "./classes/Payment.js"
-import { HasFormatter } from "./interfaces/HasFormatter.js"
+// generics
 
+const addUID = (obj: object) => {
+    let uid = Math.floor(Math.random() * 100)
+    return { ...obj, uid }
+}
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+let docOne = addUID({ name: 'mike', age: 30 })
 
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-// list template instance
-const ul = document.querySelector('ul')!
-const list = new ListTemplate(ul)
-
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-
-    let doc: HasFormatter;
-    if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-    } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    // console.log(doc);
-    list.render(doc, type.value, 'start')
-
-});
-
-
-
+console.log(docOne.name); // we cannot access the name property of the obj
