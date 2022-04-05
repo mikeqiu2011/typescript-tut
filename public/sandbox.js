2f2fedb74dc904295bf71d48623c2619d64ae3f4
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 10:36:11 +0800

    default value of arg

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,3 +1,8 @@
 "use strict";
-let char = 'mike';
-console.log(char);
+let greet;
+// optional argument
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,8 +1,9 @@
 let greet: Function;
 
 // optional argument
-const add = (a: number, b: number, c?: number | string) => {
+const add = (a: number, b: number, c: number | string = 10) => {
     console.log(a + b);
+    console.log(c);
 
 }
 
