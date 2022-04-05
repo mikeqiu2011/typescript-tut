ef1e474c5aac1e60dfda015a67665cf5463fe1d3
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 15:22:13 +0800

    extends more specific

--- public/app.js
+++ public/app.js
@@ -1,25 +1,11 @@
-import { Invoice } from "./classes/Invoice.js"; // not TS but JS, otherwise browser does not know
-import { ListTemplate } from "./classes/ListTemplate.js";
-import { Payment } from "./classes/Payment.js";
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-// list template instance
-const ul = document.querySelector('ul');
-const list = new ListTemplate(ul);
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let doc;
-    if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    // console.log(doc);
-    list.render(doc, type.value, 'start');
-});
+"use strict";
+// generics
+// by extends obj, the string passed in no longer works
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'mike', age: 30 });
+console.log(docOne.age); // now we can access the name property of the obj
+let docTwo = addUID({ age: 10 }); // this is allowed for generic though it does not make sense
+console.log(docTwo);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // generics
 
 // by extends obj, the string passed in no longer works
-const addUID = <T extends object>(obj: T) => { // it capture whatever we pass into the function
+const addUID = <T extends { name: string }>(obj: T) => { // it capture whatever we pass into the function
     let uid = Math.floor(Math.random() * 100)
     return { ...obj, uid }
 }
@@ -9,7 +9,7 @@ const addUID = <T extends object>(obj: T) => { // it capture whatever we pass in
 let docOne = addUID({ name: 'mike', age: 30 })
 console.log(docOne.age); // now we can access the name property of the obj
 
-let docTwo = addUID('mike') // this is allowed for generic though it does not make sense
+let docTwo = addUID({ age: 10 }) // this is allowed for generic though it does not make sense
 console.log(docTwo);
 
 
