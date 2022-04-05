d2f4b2449129e4ab10ed674bfa20077562d46ce3
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 15:21:06 +0800

    T extends object

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 // generics
 
-const addUID = (obj: object) => {
+// by extends obj, the string passed in no longer works
+const addUID = <T extends object>(obj: T) => { // it capture whatever we pass into the function
     let uid = Math.floor(Math.random() * 100)
     return { ...obj, uid }
 }
 
 let docOne = addUID({ name: 'mike', age: 30 })
+console.log(docOne.age); // now we can access the name property of the obj
 
-console.log(docOne.name); // we cannot access the name property of the obj
+let docTwo = addUID('mike') // this is allowed for generic though it does not make sense
+console.log(docTwo);
+
+
