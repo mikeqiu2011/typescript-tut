cc03a8d902c79ef49af0ab46f1e5007588b491bc
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 11:46:44 +0800

    get element by id and add event listener

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
 // TS has no access to html, so the anchor might be null, thus anchor may
 // not have the href, as developer, if you are certain, you can add a "!"
-const anchor = document.querySelector('a')!
-console.log(anchor.href);
+// const anchor = document.querySelector('a')!
+// console.log(anchor.href);
+
+// select by css class name and cast it into FORM
+const form = document.querySelector('.new-item-form') as HTMLFormElement
+// console.log(form.children)
+
+// select by id for the inputs
+const type = document.querySelector('#type') as HTMLSelectElement
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
+
+// add event listener
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(type.value, tofrom.value, details.value, amount.value);
+
+})
