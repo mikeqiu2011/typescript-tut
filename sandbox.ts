81537195b8837f4716afe5ab5593cecf91e9d109
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 09:33:59 +0800

    assign type but not initialize the value

--- sandbox.ts
+++ sandbox.ts
@@ -1,22 +1,42 @@
-let ninja = {
-    name: 'mike',
-    age: 30,
-    belt: 'black'
+// explicit types, when the value cannot be determined initially
+let char: string
+let age: number
+let isLoggedIn: boolean
+
+char = "mike"
+// char = 1 // not compile
+
+// below is not allowed
+// let ninjas: string[]
+// ninjas.push('mike')
+
+// below is fine
+let ninjas: string[] = []
+ninjas.push('mike')
+
+//union type of mixed array
+let mixed: (string | number | boolean)[] = []
+mixed.push(20)
+mixed.push('hello')
+mixed.push(false)
+
+let uid: string | number;
+uid = 1
+uid = '1'
+
+//obj
+let ninjaOne: object;
+ninjaOne = { name: 'mike', age: 30 }
+ninjaOne = { name: 'mike', age: 30, belt: 'black' } // work
+// ninjaOne = 'mike' //not work
+
+// explicit obj typing
+let ningjaTwo: {
+    name: string,
+    age: number,
+    belt: string
 }
 
-// ninja.name = 1 // not compile
-ninja.name = 'kevin'
+// ningjaTwo = { name: 'mike', age: 30 } // not working
+ningjaTwo = { name: 'mike', age: 30, belt: 'black' } // work
 
-ninja = { // this works
-    name: 'kevin',
-    age: 30,
-    belt: 'black',
-}
-
-// this not
-// ninja = { 
-//     name: 'kevin',
-//     age: 30,
-//     belt2: 'black',
-//     address: 'shanghai'
-// }
