e123b6e7f2d24f22c2da17b9d193c6dbc671ba5d
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 14:31:06 +0800

    interface with class

--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,15 @@
-// interfaces
-interface IsPerson {
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a: number): number;
-}
-
-const me: IsPerson = {
-    name: "mike",
-    age: 30,
-    speak(text: string): void {
-        console.log(text + this.name);
-    },
-    spend: function (amount: number): number {
-        console.log(`I spent ${amount}`);
-        return amount
-    },
-    // skills: [] // this does not compile as it not apprear in interface
-};
-
-console.log(me)
-
-const greetPerson = (person: IsPerson) => {
-    console.log('hello ', person.name);
-
-}
-
-greetPerson(me)
-
-
-// classes
 import { Invoice } from "./classes/Invoice.js" // not TS but JS, otherwise browser does not know
+import { Payment } from "./classes/Payment.js"
+import { HasFormatter } from "./interfaces/HasFormatter.js"
 
-const invOne = new Invoice('mike', 'gas fee', 100)
-const invTwo = new Invoice('kevin', 'food fee', 200)
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
 
-// invOne.client = 'eric' // readonly property cannot be modified
-// invOne.amount = 150 // property by default is public, which is not safe, after made private, you cannot access it
-
-// console.log(invOne.format());
-
-let invoices: Invoice[] = []
-invoices.push(invOne)
-invoices.push(invTwo)
-invoices.forEach(inv => console.log(inv.format()))
+docOne = new Invoice('mike', 'gas fee', 100)
+docTwo = new Payment('kevin', 'food fee', 200)
 
 
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement
-
-// select by id for the inputs
-const type = document.querySelector('#type') as HTMLSelectElement
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement
-const details = document.querySelector('#details') as HTMLInputElement
-const amount = document.querySelector('#amount') as HTMLInputElement
-
-// add event listener
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-
-})
+const docs: HasFormatter[] = []
+docs.push(docOne)
+docs.push(docTwo)
+docs.forEach(doc => console.log(doc.format()))
